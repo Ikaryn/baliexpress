a8a26515f1597b4a763e647b36031ad1fd2e6c38
marcuskmlee <z5164880@ad.unsw.edu.au>
2021-03-14 21:29:18 +1100

    Test Backend/Frontend link

--- frontend/src/pages/ProductListPage.jsx
+++ frontend/src/pages/ProductListPage.jsx
@@ -12,11 +12,7 @@ const ProductListPage = () => {
     console.log(category);
     React.useEffect(() => {
         (async () => {
-<<<<<<< HEAD
             const products = await api.get(`product/${category}`,{category: category});
-=======
-            // const products = await api.get(`product/${category}`,{catergory: category});
->>>>>>> 90a998d318b356072209c2bbfe2c0dc744d28578
             if (products.products) {
                 setProducts(products.products);
             }
