2ac5f9b21c4b935f942677be7da47b4bef38a3ec
Van-Roy Trinh <z5163773@ad.unsw.edu.au>
2021-03-14 20:48:41 +1100

    added menu dropdown and shipping information to profile page

--- frontend/src/components/AccInfoBlock.jsx
+++ frontend/src/components/AccInfoBlock.jsx
@@ -1,123 +1,151 @@
 import React from 'react';
-import { Button, Grid, Input, Typography } from '@material-ui/core';
+import { Button, Divider, Grid, Input, Typography } from '@material-ui/core';
 import API from '../util/API';
+import './styles/profilePage.css';
 
 const api = new API();
 
-const AccInfoblock = ({editComponent, name, email, phone, setEditComponent}) => {
+const AccInfoblock = ({editComponent, accInfo, shippingInfo, setEditComponent}) => {
+    
+    const [userInfo, setUserInfo] = React.useState(accInfo);
     
     const [editName, setEditName] = React.useState('');
     const [editEmail, setEditEmail] = React.useState('');
     const [editPhone, setEditPhone] = React.useState('');
-    console.log(editName, email, phone);
+    const [editAddr, setEditAddr] = React.useState('');
+    const [editCity, setEditCity] = React.useState('');
+    const [editPCode, setEditPCode] = React.useState('');
+    const [editCountry, setEditCountry] = React.useState('');
+    const [editState, setEditState] = React.useState('');
     
+    console.log(shippingInfo);
     React.useEffect(()=>{
-        setEditName(name);
-        setEditEmail(email);
-        setEditPhone(phone);
-    },[email, name, phone])
+        setUserInfo(accInfo);
+        setEditName(accInfo.name);
+        setEditEmail(accInfo.email);
+        setEditPhone(accInfo.phone);
+        setEditAddr(shippingInfo.addr);
+        setEditCity(shippingInfo.city);
+        setEditPCode(shippingInfo.pCode);
+        setEditCountry(shippingInfo.country);
+        setEditState(shippingInfo.state);
+        
+    },[accInfo, shippingInfo.addr, shippingInfo.city, shippingInfo.country, shippingInfo.pCode, shippingInfo.state])
     
-    const handleChange = (value, flag) => {
-        switch(flag) {
+    const handleChange = (value, key) => {
+        switch(key) {
             case 'name':
                 setEditName(value);
                 break;
             case 'email':
                 setEditEmail(value);
                 break;
-            default:  
+            case 'phone':  
                 setEditPhone(value);
                 break;
+            case 'addr':
+                return setEditAddr(value);;
+            case 'city':
+                return setEditCity(value);;
+            case 'pCode':
+                return setEditPCode(value);;
+            case 'country':
+                return setEditCountry(value);;
+            default:
+                return setEditState(value);;
         }
     }
     
+    const convertFieldName = (value) => {
+        switch(value) {
+            case 'name':
+                return 'Name';
+            case 'email':
+                return 'Email';
+            case 'phone':
+                return 'Phone Number';
+            case 'addr':
+                return 'Address';
+            case 'city':
+                return 'City';
+            case 'pCode':
+                return 'Postal Code';
+            case 'country':
+                return 'Country';
+            default:
+                return 'State';
+        }
+    }
+    
+    const getInformationValues = (type) => {
+        switch(type) {
+            case 'name':
+                return editName;
+            case 'email':
+                return editEmail;
+                case 'phone':
+                    return editPhone;
+                case 'addr':
+                    return editAddr;
+                case 'city':
+                    return editCity;
+                case 'pCode':
+                    return editPCode;
+                case 'country':
+                    return editCountry;
+                default:
+                    return editState;
+        }
+    }
     const handleSubmit = async () => {
         const body = {name: editName, email: editEmail, phone: editPhone};
         const response = await api.put(`profile/${localStorage.getItem('userId')}`, body);
         console.log(response);
-        console.log('hello')
         setEditComponent(false);
     }
-    
+    console.log(Object.keys(accInfo));
     return (
-        <div>
-            {editComponent === true? 
-                <Grid container item direction="column">
-                    <Grid item>
-                        <Typography variant="h3">Account information</Typography>
-                    </Grid>
-                    <Grid container item direction="row">
-                        <Grid item>
-                            <Typography variant="body2">Name:</Typography>
-                        </Grid>
-                        <Grid item>
-                            <Input 
-                                value={editName} 
-                                onChange={(event) => handleChange(event.target.value, 'name')} 
-                                variant="body2">
-                            </Input>
-                        </Grid>
-                    </Grid>
-                    <Grid container item direction="row">
-                        <Grid item>
-                            <Typography variant="body2">Email:</Typography>
-                        </Grid>
-                        <Grid item>
-                            <Input 
-                                value={editEmail} 
-                                onChange={(event) => handleChange(event.target.value, 'email')} 
-                                variant="body2"
-                            ></Input>
-                        </Grid>
-                    </Grid>
-                    <Grid container item direction="row">
-                        <Grid item>
-                            <Typography variant="body2">Phone:</Typography>
-                        </Grid>
-                        <Grid item>
-                            <Input 
-                                value={editPhone} 
-                                onChange={(event) => handleChange(event.target.value, 'phone')} 
-                                variant="body2">
-                            </Input>
-                        </Grid>
-                    </Grid>
-                    <Grid item>
-                        <Button variant="contained" onClick={() => handleSubmit()}>Confirm Changes</Button>
-                    </Grid>
-                </Grid>
-            :
             <Grid container item direction="column">
-                    <Grid item>
-                        <Typography variant="h3">Account information</Typography>
-                    </Grid>
-                    <Grid container item direction="row">
-                        <Grid item>
-                            <Typography variant="body2">Name:</Typography>
-                        </Grid>
-                        <Grid item>
-                            <Typography variant="body2">{name}</Typography>
-                        </Grid>
-                    </Grid>
-                    <Grid container item direction="row">
-                        <Grid item>
-                            <Typography variant="body2">Email:</Typography>
-                        </Grid>
-                        <Grid item>
-                            <Typography variant="body2">{email}</Typography>
-                        </Grid>
-                    </Grid>
-                    <Grid container item direction="row">
-                        <Grid item>
-                            <Typography variant="body2">Phone:</Typography>
-                        </Grid>
-                        <Grid item>
-                            <Typography variant="body2">{phone}</Typography>
-                        </Grid>
-                    </Grid>
+                <Grid item>
+                    <Typography variant="h3">Account information</Typography>
                 </Grid>
-            }
-        </div>
+                <Divider/>
+                {Object.keys(accInfo).map((value) => (
+                    <Grid container item direction="row">
+                        <Grid item  className="account-info-field">
+                            <Typography variant="h5">{convertFieldName(value) + ':'}</Typography>
+                        </Grid>
+                        <Grid item className="account-info-field">
+                            {editComponent === true ?
+                                <Input value={getInformationValues(value)} onChange={(event) => {handleChange(event.target.value, value)}} />
+                            :
+                                <Typography variant="h5">{getInformationValues(value)}</Typography>
+                            }
+                        </Grid>
+                    </Grid>
+                ))}
+                <Grid item>
+                    <Typography variant="h3">Shipping information</Typography>
+                </Grid>
+                <Divider/>
+                {Object.keys(shippingInfo).map((value) => (
+                    <Grid container item direction="row">
+                        <Grid item className="account-info-field">
+                            <Typography variant="h5">{convertFieldName(value) + ':'}</Typography>
+                        </Grid>
+                        <Grid item className="account-info-field">
+                            {editComponent === true ?
+                                <Input value={getInformationValues(value)} onChange={(event) => {handleChange(event.target.value, value)}} />
+                            :
+                                <Typography variant="h5">{getInformationValues(value)}</Typography>
+                            }
+                        </Grid>
+                    </Grid>
+                ))}
+                {editComponent ?                 <Grid item>
+                    <Button variant="contained" onClick={() => handleSubmit()}>Confirm Changes</Button>
+                </Grid> : ''}
+            </Grid>
     );
 
 }
--- frontend/src/pages/ProductListPage.jsx
+++ frontend/src/pages/ProductListPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useParams } from 'react-router';
 import API from '../util/API';
 
 const api = new API();
@@ -7,9 +8,11 @@ const ProductListPage = () => {
     
     const [products, setProducts] = React.useState([]);
     
+    let {category} = useParams();
+    console.log(category);
     React.useEffect(() => {
         (async () => {
-            const products = await api.get(`product/${category}`,{catergory: category});
+            // const products = await api.get(`product/${category}`,{catergory: category});
             if (products.products) {
                 setProducts(products.products);
             }
