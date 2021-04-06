fead64106f4a86f8f5bf9e26e4991f0cbad00041
Van-Roy Trinh <z5163773@ad.unsw.edu.au>
2021-04-06 21:41:40 +1000

    added user builds list and correctly links to build

--- frontend/src/components/profilePage/UserBuildList.jsx
+++ frontend/src/components/profilePage/UserBuildList.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import API from '../../util/API';
-import {  Paper,  Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core';
+import {  Button, Paper,  Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core';
+import { useHistory } from 'react-router';
 const api = new API();
 
 const UserBuildList = () => {
     const [builds, setBuilds] = React.useState(null);
+    const history = useHistory();
     
     React.useEffect(() => {
         (async () => {
             const response = await api.get(`userBuilds?userId=${localStorage.getItem('userId')}`);
             console.log(response);
+            console.log(response.builds);
             setBuilds(response.builds);
         })();
     },[])
@@ -22,11 +25,20 @@ const UserBuildList = () => {
                     <TableCell align="right">ID</TableCell>  
                     <TableCell align="right">Name</TableCell>
                     <TableCell align="right">Description</TableCell>
-                    <TableCell align="right">Price</TableCell>
                 </TableRow>
             </TableHead>
             <TableBody>
-                {builds.map}
+                {builds && builds.map((build) => (
+                    <TableRow key={build.buildid}>
+                        <TableCell>{build.buildid}</TableCell>
+                        <TableCell>{build.buildname}</TableCell>
+                        <TableCell>{build.description}</TableCell>
+                        <TableCell>
+                            <Button variant="contained" color="primary" onClick={() => {history.push(`/build/${build.userid}/${build.buildid}`)}}>Edit Build</Button>
+                            <Button variant="contained" color="secondary">Delete Build</Button>
+                        </TableCell>
+                    </TableRow>  
+                ))}
             </TableBody>
         </Table>
     </TableContainer>)
