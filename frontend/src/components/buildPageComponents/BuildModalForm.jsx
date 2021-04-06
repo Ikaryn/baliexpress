00651753c17db1149067570159c631d0cc6f472e
Van-Roy Trinh <z5163773@ad.unsw.edu.au>
2021-04-06 19:58:25 +1000

    added info to the build form

--- frontend/src/components/buildPageComponents/BuildModalForm.jsx
+++ frontend/src/components/buildPageComponents/BuildModalForm.jsx
@@ -1,4 +1,4 @@
-import { Button, Checkbox, Divider, FormControl, FormControlLabel, Grid, InputLabel, MenuItem, OutlinedInput, Paper, Select, Typography } from '@material-ui/core';
+import { Button, Checkbox, Divider, FormControl, FormControlLabel, FormHelperText, Grid, InputLabel, MenuItem, OutlinedInput, Paper, Select, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import React from 'react';
 import { useHistory } from 'react-router';
@@ -11,6 +11,9 @@ const useStyles = makeStyles((theme) => ({
         minWidth: 220,
         marginBottom: '1em',
     },
+    divider: {
+        marginBottom: '1em',
+    },
   }));
   
 const api = new API();
@@ -21,21 +24,57 @@ const BuildModalForm = ({handleToggle, setOpen}) => {
     const [budget, setBudget] = React.useState('');
     const [overclock, setOverClock] = React.useState(false);
     const [storage, setStorage] = React.useState('');
+    const [error, setError] = React.useState({
+        'usage': '',
+        'budget': '',
+        'storage': ''
+    });
+    
+    
     const history = useHistory();
 
     const handleUsageChange = (value) => {
         setUsage(value);
     }
     
+    const errorHandler = () => {
+        let isError = false;
+        let errorObject = JSON.parse(JSON.stringify(error));
+        if (usage === '') {
+            errorObject['usage'] = 'Please select a usage.';
+            isError = true;
+        }
+        if (budget === '') {
+            errorObject['budget'] = "Please enter a budget";
+            isError = true;
+        }
+        if (typeof budget !== 'number') {
+            errorObject['budget'] = "Budget must be a number";
+            isError = true;
+        }
+        if (storage === '') {
+            error['storage'] = "Please specify which storage";
+            isError = true;
+        }
+        
+        setError(errorObject);
+        
+        return isError;
+    }
+    
     const handleRedirect = (flag) => {
-        handleToggle(false);
         if (flag === 'empty'){ 
+            handleToggle(false);
             history.push('/build');
-        } else if (flag == 'build'){
-            history.push('/build');
-            
-            const res = api.get(`build?usage=${usage}&&budget=${budget}&&overclock=${overclock}&&storage=${storage}`)
-            console.log(usage, budget, overclock, storage)
+        } else if (flag === 'build'){
+            console.log(errorHandler());
+            if (!errorHandler()) {
+                history.push('/build');
+                const res = api.get(`build?usage=${usage}&&budget=${budget}&&overclock=${overclock}&&storage=${storage}`)
+                console.log(usage, budget, overclock, storage)
+                console.log(res);
+                handleToggle(false);
+            }
         }
     }
     
@@ -56,33 +95,47 @@ const BuildModalForm = ({handleToggle, setOpen}) => {
                 <Grid item>
                     <Typography variant="h5">PC Requirements</Typography>
                 </Grid>
-                <Divider />
+                
+                <Divider className={classes.divider}/>
+                <Grid item>
+                    <Typography>Not all PC's are built to the same specs! Please let us know what you would use it for, so we can curate a PC custom built for you.</Typography>
+                </Grid>
                 <Grid item>
                     <FormControl className={classes.formControl}>
                         <InputLabel htmlFor="useage-input">Usage</InputLabel>
-                        <Select fullWidth value={usage} onChange={(event) => handleUsageChange(event.target.value)}>
+                        <Select error={error['usage'] === '' ? false : true} fullWidth value={usage} onChange={(event) => handleUsageChange(event.target.value)}>
                             <MenuItem value='business'>Business</MenuItem>
                             <MenuItem value='gaming'>Gaming</MenuItem>
                             <MenuItem value='video'>Video Editing/Rendering</MenuItem>
                             <MenuItem value='animation'>Animation</MenuItem>
                             <MenuItem value='art'>Art</MenuItem>
                         </Select>
+                        <FormHelperText>{error['usage']}</FormHelperText>
                     </FormControl>
+                </Grid>
+                <Grid item>
+                    <Typography>Let us know how much you have to spend! This will allow us to create the most bang for your buck build.</Typography>
                 </Grid>
                 <Grid item>
                     <FormControl className={classes.formControl}>
                         <InputLabel htmlFor="budget-input">Budget</InputLabel>
-                        <OutlinedInput value={budget} onChange={event => setBudget(event.target.value)}></OutlinedInput>
+                        <OutlinedInput error={error['budget'] === '' ? false : true} value={budget} onChange={event => setBudget(event.target.value)}></OutlinedInput>
+                        <FormHelperText>{error['budget']}</FormHelperText>
                     </FormControl>
                 </Grid>
+                <Typography>Don't know what these options mean? Don't worry We'll tell you! HDD Stands for hard drives, these are more older, but the more affordable option. SSD is the newer technology allowing faster load times and more power efficiency, but at a higher cost.</Typography>
                 <Grid item>
                     <FormControl className={classes.formControl}>
                         <InputLabel>Type of Storage</InputLabel>
-                        <Select fullWidth value={storage} onChange={event => {setStorage(event.target.value)}}>
+                        <Select error={error['storage'] === '' ? false : true} fullWidth value={storage} onChange={event => {setStorage(event.target.value)}}>
                             <MenuItem value="hdd">HDD</MenuItem>
                             <MenuItem value="ssd">SSD</MenuItem>
                         </Select>
+                        <FormHelperText>{error['storage']}</FormHelperText>
                     </FormControl>
+                </Grid>
+                <Grid item>
+                    <Typography>Want to push your parts to the limit? Overclocking increases performance but reduces the lifespan of your components.</Typography>
                 </Grid>
                 <Grid item>
                     <FormControlLabel 
