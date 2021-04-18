b5c26f41ab065d70a2b5d43f6866f16f70ccce59
Darryl <darryl.liu@student.unsw.edu.au>
2021-04-18 17:24:04 +1000

    fixed getting product stats

--- backend/api/stats.py
+++ backend/api/stats.py
@@ -10,53 +10,89 @@ from datetime import timedelta
 
 class Stats(Resource):
 
-    # Getting sale statistics for a product
-    # Url formats: `stats?productId=${productId}`
+    # Getting sale statistics
+    # Url formats: 
+    # For a single product: `stats?productId=${productId}`
+    # For a specific sale: `stats?saleId=${saleId}`
     def get(self):
 
-        print('Getting product stats attempt received')
         productId = request.args.get('productId')
+        saleId = request.args.get('saleId')
 
-        productStats = {}
+        if productId is not None:
 
-        # Beginning and end date to start getting all sales from
-        startDate = date(2020, 1, 1)
-        endDate = date.today()
-        curDate = startDate
+            print('Getting product stats attempt received')
+            productId = int(productId)
+            productStats = {}
 
-        while curDate <= endDate:
-            dateString = curDate.strftime('%Y-%m-%d')
-            productStats[dateString] = [0, '', '']
-            curDate += timedelta(days=1)
-        
-        sales = db.getAllSales()
-        for sale in sales:
-            saleInfo = db.getSale(sale['id'])
-            for product in saleInfo['products']:
-                if product['productid'] == productId:
-                    start = sale['startdate']
-                    end = sale['enddate']
-                    cur = start
-                    while cur <= end:
-                        dateString = cur.strftime('%Y-%m-%d')
-                        if dateString in productStats:
-                            productStats[dateString] = [0, sale['name'], product['salepercent']]
+            # Beginning and end date to start getting all sales from
+            startDate = date(2020, 1, 1)
+            endDate = date.today()
+            curDate = startDate
 
-        orders = db.getAllOrders()       
-        for order in orders:
-            for product in order['products']:
-                if product['productid'] == productId:
+            while curDate <= endDate:
+                dateString = curDate.strftime('%Y-%m-%d')
+                productStats[dateString] = [0, '', '']
+                curDate += timedelta(days=1)
+            
+            sales = db.getAllSales()
+            for sale in sales:
+                saleInfo = db.getSale(sale['id'])
+                for product in saleInfo['products']:
+                    if product['productid'] == productId:
+                        start = sale['startdate']
+                        end = sale['enddate']
+                        cur = start
+                        while cur <= end:
+                            dateString = cur.strftime('%Y-%m-%d')
+                            if dateString in productStats:
+                                productStats[dateString] = [0, sale['name'], product['salepercent']]
 
-                    orderDate = order['date'].strftime('%Y-%m-%d')
-                    if orderDate in productStats and order['date'] >= startDate:
-                        productStats[date][0] += product['quantity']
-                    else:
-                        productStats[date][0] = product['quantity']
-        
-        stats = []
-        
-        for key in productStats:
-            entry = productStats[key]
-            stats.append({'date': key, 'sold': entry[0], 'saleName': entry[1], 'salePercent': entry[2]})
+            orders = db.getAllOrders()       
+            for order in orders:
+                for product in order['products']:
+                    if product['productid'] == productId:
 
-        return {'stats': stats}
+                        orderDate = order['date'].strftime('%Y-%m-%d')
+                        if orderDate in productStats and order['date'] >= startDate:
+                            productStats[orderDate][0] += product['quantity']
+                        else:
+                            productStats[orderDate][0] = product['quantity']
+            
+            stats = []
+
+            for key in productStats:
+                entry = productStats[key]
+                stats.append({'date': key, 'sold': entry[0], 'saleName': entry[1], 'salePercent': entry[2]})
+
+            return {'stats': stats}
+
+        else:
+            print('Getting sale stats attempt received')
+            saleId = int(saleId)
+            sale = db.getSale(saleId)
+
+            saleStats = {}
+
+            startDate = sale['startdate']
+            endDate = sale['enddate']
+            curDate = startDate
+            while curDate <= endDate:
+                saleStats[curDate.strftime('%Y-%m-%d')] = 0
+
+            saleProducts = []
+            for product in sale['products']:
+                saleProducts.append(product['productid'])
+
+            orders = db.getAllOrders()
+            for order in orders:
+                if order['date'] >= startDate and order['date'] <= endDate:
+                    for product in order['products']:
+                        if product['productid'] in saleProducts:
+                            saleStats[order['date'].strftime('%Y-%m-%d')] += product['quantity']
+            
+            stats = []
+            for key in saleStats:
+                stats.append({'date': key, 'sold': saleStats[key]})
+            
+            return {'stats': stats}
