3811c682f53a900f0308850271380d9bd64fe8e3
Darryl <darryl.liu@student.unsw.edu.au>
2021-04-20 22:26:41 +1000

    discontinue product modal

--- frontend/src/components/AllProductList.jsx
+++ frontend/src/components/AllProductList.jsx
@@ -14,7 +14,7 @@ const AllProductList = () => {
     const [productStatsOpen, setProductStatsOpen] = React.useState(false);
     const [viewedProduct, setViewedProduct] = React.useState(null);
     const [openModal, setOpenModal] = React.useState(false);
-    const [discontinuedId, setDiscontinuedId] = React.useState();
+    const [discontinued, setDiscontinued] = React.useState(null);
     const [success, setSuccess] = React.useState(false);
     
     React.useEffect(() => {
@@ -32,22 +32,22 @@ const AllProductList = () => {
     },[]);
 
     const handleDiscontinue = async () => {
-        console.log("Product with ID", discontinuedId, "would be discontinued");
+        console.log("Product with ID", discontinued.id, "would be discontinued");
 
-        const response = await api.delete(`product?productId=${discontinuedId}`);
+        const response = await api.delete(`product?productId=${discontinued.id}`);
         console.log(response);
         if (response.message) {
 
             // For each category apply the filter to remove the discontinued product
             let newProducts = []
             products.forEach((category) => {
-                newProducts.push(category.filter((product) => product.id !== discontinuedId));
+                newProducts.push(category.filter((product) => product.id !== discontinued.id));
             });
 
             console.log(products);
         
             setOpenModal(false);
-            setDiscontinuedId();
+            setDiscontinued(null);
             setSuccess(true);
             setProducts(newProducts);
         }
@@ -69,19 +69,33 @@ const AllProductList = () => {
                                     setProduct={setViewedProduct} 
                                     setOpen={setProductStatsOpen} 
                                     setOpenModal={setOpenModal}
-                                    setDiscontinuedId={setDiscontinuedId}
+                                    setDiscontinued={setDiscontinued}
                                 />
                             </Grid>
                         ))}
                         </Grid>
                     ))}
-                <Modal open={openModal} onClose={() => setOpenModal(false)}>
-                    <Paper>
-                        <Typography>Hello There</Typography>
-                        <Typography>Are you sure you want to discontinue this product?</Typography>
-                        <Button variant="primary" onClick={() => handleDiscontinue()}>Yeah go for it</Button>
-                    </Paper>    
-                </Modal>
+                <Grid item container xs={5}>
+                    {discontinued &&
+                        <Modal open={openModal} onClose={() => setOpenModal(false)}>
+                            <Paper className="modal-container">
+                                <Typography variant="h5">Are you sure you want to discontinue this product?</Typography>
+                                <Grid container direction="row" alignItems="center">
+                                    <Grid item xs={4}>
+                                        <img src={"data:image/jpeg;base64," + discontinued.image} alt="product" class="product-card-image"/>
+                                    </Grid>
+                                    <Grid item xs={6}>
+                                        <Typography variant="h6">{discontinued.name}</Typography>
+                                        <Typography variant="h5">${discontinued.price}</Typography>
+                                        <Typography variant="h5">Stock: {discontinued.stock}</Typography>
+                                    </Grid>
+                                </Grid>
+                                <Button variant="contained" color="primary" onClick={() => {setOpenModal(false); setDiscontinued(null)}}>Cancel</Button>
+                                <Button variant="contained" color="secondary" onClick={() => handleDiscontinue()}>Discontinue product</Button>
+                            </Paper>    
+                        </Modal>
+                    }
+                </Grid>
                 <Snackbar open={success} autoHideDuration={1000} onClose={() => setSuccess(false)}>
                     <Alert severity="success">Product has been discontinued</Alert>
                 </Snackbar>
